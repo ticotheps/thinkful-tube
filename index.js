7e94aa4cb732a98bd6948465141e2ca4f8d50dfa
Tico S. Thepsourinthone <ticotheps@gmail.com>
2018-10-05 17:45:20 -0400

    Added getYouTubeData function to index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
 $(document).ready(function(){
-  console.log("The web page has successfully loaded.");
+  console.log('The web page has successfully loaded.');
+
+  function getYouTubeData(searchTerm, callback) {
+    const settings = {
+      url: YOUTUBE_SEARCH_URL,
+      part: 'snippet',
+      key: 'AIzaSyCgDg7VJayp6ckpidBxekWDs-Ur1u3sipw',
+      data: {
+        q: `${searchTerm} in:name`,
+        per_page: 7
+      },
+      dataType: 'json',
+      type: 'GET',
+      success: callback
+    };
+
+    $.ajax(settings);
+  }
 })
