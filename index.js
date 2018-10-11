a8f5fade0188132eba203b263a3ce78e6f6124d6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2018-10-11 08:39:03 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 var myKey = config.MY_KEY;
 
 $(document).ready(function(){
   console.log("The web page has successfully loaded.");
 
-  //  This hides the RESULTS divs upon initial loading of the page.
-  $('.js-search-section').hide();
+  //  This hides the RESULTS section element upon initial loading of the page.
+  $(".js-search-section").hide();
 
   //  This prevents the <form> element from default submission behavior.
   $(document).on('submit', '.js-search-form', function(event){
@@ -21,31 +21,50 @@ $(document).ready(function(){
     //  This clears out the search input box.
     searchQuery.val("");
 
+    getYouTubeData(searchWords, displayYouTubeData);
   });
 
 });
 
-//  This listens for submissions from the <form> element.
-// function listenSubmit() {
-//   $('.js-search-form').submit(function() {
-//     console.log('Something was submitted.');
-//   });
-//
-//   listenSubmit()
+  function getYouTubeData(searchTerm, callback) {
+    console.log("The getYouTubeData function is running.");
 
-  // function getYouTubeData(searchTerm, callback) {
-  //   const settings = {
-  //     url: YOUTUBE_SEARCH_URL,
-  //     part: 'snippet',
-  //     key: myKey
-  //     data: {
-  //       q: `${searchTerm} in:name`,
-  //       per_page: 7
-  //     },
-  //     dataType: 'json',
-  //     type: 'GET',
-  //     success: callback
-  //   };
-  //
-  //   $.ajax(settings);
-  // }
+    const settings = {
+      url: YOUTUBE_SEARCH_URL,
+      data: {
+        part: "snippet",
+        key: myKey,
+        q: `${searchTerm} in:name`,
+        per_page: 5
+      },
+      dataType: 'json',
+      type: 'GET',
+      success: callback
+    };
+
+    $.ajax(settings);
+  }
+
+  function displayYouTubeData(data) {
+    console.log(`The API was successful in retrieving data and the
+      displayYouTubeData function is running.`);
+
+    //  This displays the RESULTS section element on the page.
+    $(".js-search-section").show();
+
+    const results = data.items.map(item, index);
+    console.log(results);
+
+    // $('.js-search-results').html(results);
+  }
+
+  // function renderResult(result) {
+  //   return `
+  //   <div>
+  //     <h2>
+  //     <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+  //     <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
+  //     <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+  //   </div>
+  // `;
+  // };
